3a40377e2cce55ad0a22f657f4a848f542cccc46
Nathan <nathandyzh@gmail.com>
2025-02-06 12:27:30 -0500

    Created sum.cpp
    Calculating sum of the first 100 natural numbers

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+// sum.cpp
+#include <iostream>
+
+int main() {
+    int sum = 0;
+    for (int i = 1; i <= 100; ++i) {
+        sum += i;
+    }
+    std::cout << "The sum of the first 100 natural numbers is: " << sum << std::endl;
+    return 0;
+}
+
