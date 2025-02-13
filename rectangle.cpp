a0a95b2340cd133cee58dc97c6ebf049432631b0
Nathan <nathandyzh@gmail.com>
2025-02-13 17:54:58 -0500

    Added instructions
    Changed way of commenting instructions

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <string>
 
-// In your C++ folder, create a new file called "rectangle.cpp". In this file, create a main function as usual. Then create a class called "Rectangle." It should have as attributes "length" and "width" (both integers). Develop the class using the design principles we have been using to design classes (e.g., the attributes should be private variables, and there should be at least one constructor and public getter and setter methods for each attribute). Then, create a public method "getArea()" that calculates and returns the area of the rectangle. Use the Dog class we have been working on as a template to help you write the code for the Rectangle class. Then, in the main function, create a Rectangle object named "rectangle" with length = 5 and width = 3. Print out the area of rectangle by printing the value returned by its "getArea()" method. Make sure to have "#include <string>" and "#include <iostream>" at the top of the file.
-
+/* In your C++ folder, create a new file called "rectangle.cpp". In this file, create a main function as usual. Then create a class called "Rectangle." 
+It should have as attributes "length" and "width" (both integers). Develop the class using the design principles we have been using 
+to design classes (e.g., the attributes should be private variables, and there should be at least one constructor and public getter and setter methods for each attribute). 
+Then, create a public method "getArea()" that calculates and returns the area of the rectangle. 
+Use the Dog class we have been working on as a template to help you write the code for the Rectangle class. 
+Then, in the main function, create a Rectangle object named "rectangle" with length = 5 and width = 3. 
+Print out the area of rectangle by printing the value returned by its "getArea()" method. 
+Make sure to have "#include <string>" and "#include <iostream>" at the top of the file.
+*/
 class Rectangle {
 private:
     int length;
